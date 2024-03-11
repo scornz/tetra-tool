ffc715d916659dff4cc3105d8ef54a646c04d3c1
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-03-11 12:58:21 -0700

    Added basic Vector2 class

---
+++ src/game/engine/Vector2.ts
@@ -0,0 +1,10 @@
+/**
+ * A 2D vector. Supports basic vector operations.
+ */
+class Vector2 {
+  constructor(
+    public readonly x: number = 0.0,
+    public readonly y: number = 0.0
+  ) {}
+}
+export default Vector2;
