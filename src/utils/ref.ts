32633b49f213fffdd11a47b2b9c92f99a3e0fd51
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-03-13 13:03:19 -0700

    Added special hook to use nonnull handle refs

--- src/utils/ref.ts
+++ src/utils/ref.ts
@@ -1,3 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
 export type HandleObject<T> = {
   grab: () => T;
+};
+
+/**
+ * Use a ref object that is guaranteed to be non-null.
+ * @returns
+ */
+export const useNonnullHandleRef = <T>(): [
+  React.RefObject<HandleObject<T>>,
+  T | null
+] => {
+  const ref = useRef<HandleObject<T>>(null);
+  const [val, setVal] = useState<T | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) {
+      throw new Error("Expected ref.current to be non-null");
+    }
+
+    setVal(ref.current.grab());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return [ref, val];
 };
