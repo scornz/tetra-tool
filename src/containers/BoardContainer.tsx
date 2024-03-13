5a3d2c742eca15aa9eec5590a098a536103954ec
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-03-13 11:17:35 -0700

    Exposed instance of Board in component

--- src/containers/BoardContainer.tsx
+++ src/containers/BoardContainer.tsx
@@ -1,28 +1,36 @@
 import { Box, PropsOf, styled } from "@chakra-ui/react";
 import { Board } from "@/game/objects";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, useImperativeHandle } from "react";
 import { useEngine } from "@/state/engine";
+import { HandleObject } from "@/utils";
 
-const BoardContainer = ({ ...props }: PropsOf<typeof Box>) => {
+const BoardContainer = (
+  { ...props }: PropsOf<typeof Box>,
+  ref: React.ForwardedRef<HandleObject<Board | null>>
+) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [board, setBoard] = useState<Board | null>(null);
   const engine = useEngine();
+
+  useImperativeHandle(ref, () => ({
+    grab: () => board,
+  }));
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const board = null;
     if (canvas) {
       // Access the canvas context and perform any drawing operations here
       const ctx = canvas.getContext("2d");
-
       if (ctx) {
         const board = new Board(engine, ctx, 10, 30, 20, true);
+        setBoard(board);
         // Example: Draw a red rectangle
         board.draw();
       }
     }
 
     return () => {};
-  }, []);
+  }, [engine]);
 
   return (
     <Box {...props}>
@@ -36,4 +44,5 @@ const BoardContainer = ({ ...props }: PropsOf<typeof Box>) => {
   );
 };
 
-export default BoardContainer;
+const ForwardedBoardContainer = React.forwardRef(BoardContainer);
+export default ForwardedBoardContainer;
---
+++ src/utils/ref.ts
@@ -0,0 +1,3 @@
+export type HandleObject<T> = {
+  grab: () => T;
+};
