8fb926f04f9b9ca4abd91915c7786de4fe4191e5
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-03-24 14:11:33 -0400

    Added Hold canvas element

---
+++ src/game/objects/Hold.ts
@@ -0,0 +1,60 @@
+import { getCoords } from "@/utils";
+import {
+  BOARD_COLORS,
+  TETROMINO_PREVIEW_SHAPES,
+  TetrominoType,
+} from "@/game/constants";
+import { CanvasEntity, Engine } from "@/game/engine";
+
+class Hold extends CanvasEntity {
+  /**
+   * The current held piece, if there is one.
+   */
+  private piece: TetrominoType | null = null;
+
+  constructor(engine: Engine, ctx: CanvasRenderingContext2D) {
+    super(engine, ctx);
+    this.ctx = ctx;
+  }
+
+  update(_delta: number): void {}
+
+  set(type: TetrominoType) {
+    this.piece = type;
+    this.draw();
+  }
+
+  draw() {
+    // Clear board prior to every draw
+    this.clear();
+
+    if (!this.piece) return;
+
+    const shape = TETROMINO_PREVIEW_SHAPES[this.piece];
+    for (let y = 0; y < shape.length; y++) {
+      for (let x = 0; x < shape[y].length; x++) {
+        const cell = shape[y][x];
+        if (cell) {
+          this.drawCell(x, y, cell);
+        }
+      }
+    }
+  }
+
+  /**
+   * Draw a cell on the board
+   * @param x The x position of the cell
+   * @param y The y position of the cell
+   * @param type The type of tetromino to draw
+   * @param clear Whether to clear the cell
+   */
+  private drawCell = (x: number, y: number, type: TetrominoType) => {
+    const cellSize = this.ctx.canvas.width / 4;
+    const coords = getCoords(this.ctx.canvas, x * cellSize, y * cellSize);
+    this.ctx.fillStyle = BOARD_COLORS[type];
+    // Draw with negative cell y-size because the y-axis is inverted
+    this.ctx.fillRect(coords.x, coords.y, cellSize, -cellSize);
+  };
+}
+
+export default Hold;
