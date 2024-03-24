b49861bdcb19be5156ded065acd43c95cbd4e035
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-03-24 14:25:47 -0400

    Added clear lines check

--- src/game/objects/Board.ts
+++ src/game/objects/Board.ts
@@ -117,7 +117,41 @@ class Board extends CanvasEntity {
     }
     // Remove this tetromino from the scene
     tetromino.destroy();
+
+    // Check cleared lines
+    const cleared = this.checkLines();
+
     this.tetromino = null;
+  }
+
+  /**
+   * Check if any lines have been completed, and if so, remove them and shift the
+   * blocks above them appropriately. Return number of lines cleared.
+   */
+  checkLines(): number {
+    let cleared = 0;
+    for (let y = 0; y < this.layout.length; y++) {
+      let filled = true;
+      // If any of the cells in this line are empty, then this line is not filled
+      for (let x = 0; x < this.layout[y].length; x++) {
+        if (this.layout[y][x] == 0) {
+          filled = false;
+          break;
+        }
+      }
+
+      if (!filled) continue;
+
+      // Remove this line from layout and cells
+      this.layout.splice(y, 1);
+      // Replace new line of zeroes and null values along the top
+      this.layout.push(Array(this.width).fill(0));
+      // Check this line again since everything just shifted down
+      y--;
+      cleared++;
+    }
+
+    return cleared;
   }
 }
 
