f14bf68961db71114a81bc72f6d0692bd5335fd2
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-03-27 10:36:51 -0400

    Created outline for prediction algorithm

---
+++ src/game/alg/predict.ts
@@ -0,0 +1,14 @@
+/**
+ * Given a board state and a tetromino, get the set of all possible board states
+ * that could result from placing the tetromino. This includes all hard-drop possibilities,
+ * soft-drop, and all possible rotations.
+ * @param board The current board state
+ * @param tetromino The next tetromino to place
+ * @returns A set of possible boards that could result from placing the tetromino
+ */
+export const getPossibleBoards = (
+  board: number[][],
+  tetromino: number
+): number[][][] => {
+  return [];
+};
